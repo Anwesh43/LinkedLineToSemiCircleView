dd568900434207ab274dd02a54dba06fae760745
Anwesh43 <anweshthecool0@gmail.com>
2019-03-28 11:20:14 +0530

    adding LTSCNode class

--- linetosemicircleview/src/main/java/com/anwesh/uiprojects/linetosemicircleview/LineToSemiCircleView.kt
+++ linetosemicircleview/src/main/java/com/anwesh/uiprojects/linetosemicircleview/LineToSemiCircleView.kt
@@ -113,8 +113,52 @@ class LineToSemiCircleView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class LTSCNode(var i : Int, val state : State = State()) {
+
+        private var next : LTSCNode? = null
+        private var prev : LTSCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LTSCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTSCNode {
+            var curr : LTSCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
