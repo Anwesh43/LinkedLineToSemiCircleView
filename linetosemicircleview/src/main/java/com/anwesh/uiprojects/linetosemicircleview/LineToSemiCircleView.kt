37f5c2a9cf0650631df0daf8803c8c352a17fb2e
Anwesh43 <anweshthecool0@gmail.com>
2019-03-28 11:37:35 +0530

    adding LineToSemiCircle class

--- linetosemicircleview/src/main/java/com/anwesh/uiprojects/linetosemicircleview/LineToSemiCircleView.kt
+++ linetosemicircleview/src/main/java/com/anwesh/uiprojects/linetosemicircleview/LineToSemiCircleView.kt
@@ -161,4 +161,27 @@ class LineToSemiCircleView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineToSemiCircle(var i : Int) {
+
+        private val root : LTSCNode = LTSCNode(0)
+        private var curr : LTSCNode = root
+        private var dir : Int = 1
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
