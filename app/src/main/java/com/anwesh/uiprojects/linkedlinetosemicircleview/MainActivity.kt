9eeb2508f398e26bb65fa533f295ae62db67632e
Anwesh43 <anweshthecool0@gmail.com>
2019-03-29 03:07:50 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedlinetosemicircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinetosemicircleview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedlinetosemicircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.linetosemicircleview.LineToSemiCircleView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LineToSemiCircleView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
